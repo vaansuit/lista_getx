e61c0110e25b11aee2b10645fd2314a32949d139
vaansuit <vvansuit@outlook.com>
2023-04-27 15:47:46 -0300

    feat: configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lista_de_compras/home_page.dart';
 import 'package:lista_de_compras/login_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
